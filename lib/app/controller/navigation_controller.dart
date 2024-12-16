fcfc0c8137d8cc4e01f5d9f557ebfba37a82e30b
Ayesh Kumara <gammedaliyanageayesh@gmail.com>
2024-12-16 23:19:45 +0530

    add navigation bar and carsoul slider

---
+++ lib/app/controller/navigation_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class NavigationController extends GetxController {
+  var selectedIndex = 0.obs;
+  void changedIndex(int index) {
+    selectedIndex.value = index;
+  }
+}
